129ba9700103360ad929ed449de185363bae496a
Moshir <alexandermoshir@yahoo.com>
2022-09-07 19:17:19 -1000

    Began hw2

--- homework2/homework2.c
+++ homework2/homework2.c
@@ -39,24 +39,12 @@
 
 int main(int argc, char* argv[])
 {
-    printf("The second homework for ICS 212. Takes an int input 
-          \nfrom the user and outputs from 0 up until that number 
-          \nand identifies if each number is a multiple of 5.");
+    int number;
 
-    int intInput;
-    printf("Enter a maximum number to show: ");
-    scanf("%d", &intInput);
+    number = user_interface();
+    print_table(number);
     
-    print("Number    Multiple of 5?");
-    for (i = 0; i <= intInput; ++i)
-    {
-        printf(i);
 
-        if (i % 5 == 0);
-            print("    Yes\n");   
-        else
-            print("    No\n");
-    }
 
 
 }
@@ -77,5 +65,74 @@ int main(int argc, char* argv[])
 
 int user_interface()
 {
-    ...
+    int intInput = 0;
+    printf("The second homework for ICS 212. Takes an int input \nfrom the user and outputs from 0 up until that number \nand identifies if each number is a multiple of 5.\n");
+    
+    do
+    {
+        printf("Enter a maximum number to show: ");
+        scanf("%d", &intInput);
+    } 
+    while (!(intInput > 0));
+    {
+        printf("That wasn't a valid number! Please try again: ");
+        scanf("%d", &intInput);
+    }
+
+    return intInput;
+}
+
+/*****************************************************************
+//
+//  Function name: is_multiple4
+//
+//  DESCRIPTION:   A template function
+//                 This function does not do anything.
+//                 Please describe your function correctly.
+//
+//  Parameters:    int input
+//
+//  Return values:  1 : input is a multiple of 4
+                    0 : input is not a multiple of 4
+//
+****************************************************************/
+
+int is_multiple4(int input)
+{
+    if ( input % 4 == 0)
+    {
+        return 1;
+    }
+    else
+        return 0;
+}
+
+/*****************************************************************
+//
+//  Function name: print_table
+//
+//  DESCRIPTION:   A template function
+//                 This function does not do anything.
+//                 Please describe your function correctly.
+//
+//  Parameters:    int input
+//
+//  Return values:  0 : success
+//
+****************************************************************/
+
+int print_table(int input)
+{
+    int i;
+	printf("%*s\t", 3, "Number");
+	printf("%*s\n", 3, "Multiple of 4?");
+    for (i = 0; i < input; ++i)
+    {
+        printf("%*s\t", 3, "%d", i);
+        if (is_multiple4(i))
+            printf("%*s\n", 3, "Yes");
+        else
+            printf("%*s\n", 3, "No");
+    }
+
 }
