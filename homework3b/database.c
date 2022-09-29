3787627285dc441f7c96540edaeccfea8313a914
Moshir <alexandermoshir@gmail.com>
2022-09-28 17:13:23 -1000

    Continued hw3b

--- homework3b/database.c
+++ homework3b/database.c
@@ -1,4 +1,30 @@
+#include <stdio.h>
 void addRecord (struct record **, int, char [ ],char [ ]);
 void printAllRecords(struct record *);
 int findRecord (struct record *, int);
 int deleteRecord(struct record **, int);
+
+int main(int argc, char* argv[])
+{
+
+}
+
+void addRecord (struct record **r, int a, char b, char c) 
+{
+    
+}
+
+void printAllRecords(struct record *r) 
+{
+
+}
+
+int findRecord (struct record *r, int a) 
+{
+    return 0;
+}
+
+int deleteRecord(struct record **r, int a) 
+{
+    return 0;
+}
