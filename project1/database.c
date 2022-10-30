a5a5a20229f0973d8923910f1038f673ed35018d
Moshir <alexandermoshir@gmail.com>
2022-10-29 17:10:39 -1000

    Started Project1

---
+++ project1/database.c
@@ -0,0 +1,73 @@
+
+
+
+/*****************************************************************
+//
+//  NAME:        Alexander Moshir
+//
+//  HOMEWORK:    Project 1
+//
+//  CLASS:       ICS 212
+//
+//  INSTRUCTOR:  Ravi Narayan
+//
+//  DATE:        October 29, 2022
+//
+//  FILE:        database.c
+//
+//  DESCRIPTION:
+//   The first project for ICS 212 consists of database.c, database.h,
+//   user_interface.c, record.h, Makefile, testplan.xlsx, summary.txt,
+//   proof.txt, and output.txt
+//
+****************************************************************/
+
+#include <stdio.h>
+void addRecord(struct record **, int, char [ ], char [ ]);
+void printAllRecords(struct record *);
+int findRecord(struct record *, int);
+int deleteRecord(struct record **, int);
+int writefile(struct record *, char []);
+int readfile(struct record **, char []);
+void cleanup(struct record **);
+
+
+/*****************************************************************
+//
+//  Function name: main
+//
+//  DESCRIPTION:   Calls for a user input and prints out a table      
+//
+//  Parameters:    argc (int) : The number of elements in argv
+//                 argv (char*[]) : An array of arguments passed
+//                                  to the program.
+//
+//  Return values:  0 : success           
+//
+****************************************************************/
+
+int main(int argc, char* argv[])
+{
+
+
+}
+
+
+void addRecord(struct record** in, int x, char* name, char* address)
+{
+
+}
+
+
+
+void printAllRecords()
+{
+
+}
+
+
+
+int print_table(int input)
+{
+
+}
---
+++ project1/user_interface.c
@@ -0,0 +1,72 @@
+/*****************************************************************
+//
+//  NAME:        Alexander Moshir
+//
+//  HOMEWORK:    3b
+//
+//  CLASS:       ICS 212
+//
+//  INSTRUCTOR:  Ravi Narayan
+//
+//  DATE:        September 24, 2022
+//
+//  FILE:        homework3b.c
+//
+//  DESCRIPTION:
+//   
+//   
+//   
+//
+****************************************************************/
+
+#include <stdio.h>
+
+
+/*****************************************************************
+//
+//  Function name: main
+//
+//  DESCRIPTION:   Calls for a user input and prints out a table      
+//
+//  Parameters:    argc (int) : The number of elements in argv
+//                 argv (char*[]) : An array of arguments passed
+//                                  to the program.
+//
+//  Return values:  0 : success           
+//
+****************************************************************/
+
+int main(int argc, char* argv[])
+{
+    struct record * start = NULL;
+    
+
+    return 0;
+
+}
+
+int add(struct record **r, int a, char b, char c)
+{
+    addRecord(r, a, b, c);
+    return 0;
+}
+
+int printall()
+{
+    return 0;
+}
+
+int find()
+{
+    return 0;
+}
+
+int delete()
+{
+    return 0;
+}
+
+int quit()
+{
+    return 0;
+}
