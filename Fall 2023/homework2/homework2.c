6674f55e4397d7605cffed6609b6bf33d3ce7193
Moshir <alexandermoshir@yahoo.com>
2023-09-06 18:10:32 -1000

    Started homework 2

---
+++ Fall 2023/homework2/homework2.c
@@ -0,0 +1,40 @@
+/*****************************************************************
+//
+//  NAME:        Alexander Moshir
+//
+//  HOMEWORK:    homework 2
+//
+//  CLASS:       ICS 212
+//
+//  INSTRUCTOR:  Ravi Narayan
+//
+//  DATE:        9 September 2023
+//
+//  FILE:        homework2.c
+//
+//  DESCRIPTION: Generates a table up to user inputted number
+//               and labels them even or odd.
+//
+****************************************************************/
+
+#include <stdio.h>
+
+/*****************************************************************
+//
+//  Function name: main
+//
+//  DESCRIPTION:   Generates a table up to user inputted number
+//                 and labels them even or odd.
+//
+//  Parameters:    argc (int) : The number of elements in argv
+//                 argv (char*[]) : An array of arguments passed
+//                                  to the program.
+//
+//  Return values:  0 : Success
+//
+****************************************************************/
+
+int main(int argc, char* argv[])
+{
+    
+}
---
+++ Fall 2023/template.c
@@ -0,0 +1,59 @@
+/*****************************************************************
+//
+//  NAME:        Alexander Moshir
+//
+//  HOMEWORK:    
+//
+//  CLASS:       ICS 212
+//
+//  INSTRUCTOR:  Ravi Narayan
+//
+//  DATE:        
+//
+//  FILE:        template.c
+//
+//  DESCRIPTION:
+//   Describe the file
+//
+****************************************************************/
+
+#include <stdio.h>
+
+/*****************************************************************
+//
+//  Function name: main
+//
+//  DESCRIPTION:   Don't forget to describe what your main
+//                 function does.
+//
+//  Parameters:    argc (int) : The number of elements in argv
+//                 argv (char*[]) : An array of arguments passed
+//                                  to the program.
+//
+//  Return values:  0 : Success
+//
+****************************************************************/
+
+int main(int argc, char* argv[])
+{
+    
+}
+
+/*****************************************************************
+//
+//  Function name: foo
+//
+//  DESCRIPTION:   A template function
+//                 This function does not do anything.
+//                 Please describe your function correctly.
+//
+//  Parameters:    bar (int) : Describe the meaning
+//
+//  Return values:  0 : Success
+//
+****************************************************************/
+
+int foo(int bar)
+{
+    
+}
