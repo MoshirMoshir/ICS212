da5945798bc2aa989f735192e7677a7577ec2ad1
Moshir <alexandermoshir@yahoo.com>
2023-09-06 19:14:18 -1000

    Completed homework 2

--- Fall 2023/homework2/homework2.c
+++ Fall 2023/homework2/homework2.c
@@ -18,6 +18,9 @@
 ****************************************************************/
 
 #include <stdio.h>
+int user_interface();
+int is_even(int);
+void print_table(int);
 
 /*****************************************************************
 //
@@ -36,5 +39,103 @@
 
 int main(int argc, char* argv[])
 {
-    
+    int max = user_interface();
+
+    print_table(max);
+
+    return 0;
+}
+
+/*****************************************************************
+//
+//  Function name: user_interface();
+//
+//  DESCRIPTION:   prints instructions and takes user input
+//
+//  Parameters:    none
+//
+//  Return values:  in : number inputted by user
+//
+****************************************************************/
+
+int user_interface()
+{
+    printf("This program will generate a table of numbers up to the number you enter and label them even or odd.\n");
+    printf("Enter maximum number to show: ");
+
+    int in = 0;
+    scanf("%d", &in);
+
+    // check it input is valid positive number
+    char temp[999];
+    while(in <= 0)
+    {
+        printf("Invalid input. Please enter a positive number: ");
+        fgets(temp, 999, stdin);
+        scanf("%d", &in);
+        
+    }
+
+    return in;
+
+}
+
+/*****************************************************************
+//
+//  Function name: is_even();
+//
+//  DESCRIPTION:   checks if parameter is even
+//
+//  Parameters:    int num : number to check
+//
+//  Return values:  1 : even
+//                  0 : odd
+//
+****************************************************************/
+
+int is_even(int num)
+{
+    int out;
+
+    if(num % 2 == 0)
+    {
+        out = 1;
+    }
+    else
+    {
+        out = 0;
+    }
+
+    return out;
+}
+
+/*****************************************************************
+//
+//  Function name: print_table();
+//
+//  DESCRIPTION:   generates table of numbers up to parameter
+//
+//  Parameters:    int end : number to end table at
+//
+//  Return values:  0 : Success
+//
+****************************************************************/
+
+void print_table(int end)
+{
+    int temp = 0;
+
+    printf("Number\tEven or Odd?\n");
+    while(temp <= end)
+    {
+        if(is_even(temp) == 1)
+        {
+            printf("%6d\tEven\n", temp);
+        }
+        else
+        {
+            printf("%6d\tOdd\n", temp);
+        }
+        temp++;
+    }
 }
