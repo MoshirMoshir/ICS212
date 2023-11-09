8ffc2f7d56b3be232d6b350fd9330f83d195f660
Moshir <alexandermoshir@yahoo.com>
2023-11-08 19:53:41 -1000

    Update homework8.c

--- Fall 2023/homework8/homework8.c
+++ Fall 2023/homework8/homework8.c
@@ -28,6 +28,24 @@ void makeheader(const unsigned char[], unsigned char[]);
 
 /*****************************************************************
 //
+//  Function name: main
+//
+//  DESCRIPTION:   Tests possible data to writefile and readfile          
+//
+//  Parameters:    argc (int) : The number of elements in argv
+//                 argv (char*[]) : An array of arguments passed
+//                                  to the program.
+//
+//  Return values:  0 : success
+//                 
+****************************************************************/
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
+/*****************************************************************
+//
 //  Function name: readfile();
 //
 //  DESCRIPTION:   reads a file (database_data) to fill database  
@@ -41,21 +59,15 @@ void makeheader(const unsigned char[], unsigned char[]);
 ****************************************************************/
 int readfile(const char a[], unsigned char b[])
 {
-    FILE *fp;
+    FILE *fp = fopen("INPUT", "r");
     int bytes_read;
 
-    fp = fopen(filename, "r");
-    if (!fp) {
-        perror("fopen failed");
-        return 1;
-    }
-
-    bytes_read = fread(&my_header, sizeof(my_header), 1, fp);
+    bytes_read = fread(b, sizeof(a) + 1, 1, fp);
     if (bytes_read == -1) {
         perror("fread failed");
         fclose(fp);
         return 1;
-    } else if (bytes_read != sizeof(my_header)) {
+    } else if (bytes_read != sizeof(a)) {
         fprintf(stderr, "expected to read %d bytes, actually read %d\n", sizeof(my_header), bytes_read);
         fclose(fp);
         return 1;
@@ -72,10 +84,31 @@ int writefile(const char a[], const unsigned char b[])
 
 void printheader(const unsigned char a[])
 {
+    int i;
+    unsigned int sequence;
+    unsigned int acknowledgement;
     //print source port (decimal number)
+    for (i = 0; i < 16; i++)
+    {
+        printf("%d", a[i]);
+    }
     //print destination port (decimal number)
-    //pring sequence number ((unsigned) decimal number)
-    //print ackowledgement number ((unsigned) decimal number)
+    for (i = 16; i < 32; i++)
+    {
+        printf("%d", a[i]);
+    }
+    //print sequence number ((unsigned) decimal number)
+    for (i = 32; i < 64; i++)
+    {
+        sequence += a[i];
+    }
+    printf("%u", sequence);
+    //print acknowledgement number ((unsigned) decimal number)
+    for (i = 64; i < 96; i++)
+    {
+        acknowledgement += a[i];
+    }
+    printf("%u", acknowledgement);
     // print control bits
 }
 
